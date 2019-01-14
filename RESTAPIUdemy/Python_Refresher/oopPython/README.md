f4240d16bb06b1c606cde2604df911a841c1613a
josephsmartinez <josephsmartinez@gmail.com>
2019-01-13 20:37:45 -0500

    OOP Python

---
+++ RESTAPIUdemy/Python_Refresher/oopPython/README.md
@@ -0,0 +1,20 @@
+# OOP Python
+
+## Class Method
+
+- A class method is a method which is bound to the class and not the object of the class.
+- They have the access to the state of the class as it takes a class parameter that points to the class and not the object instance.
+- It can modify a class state that would apply across all the instances of the class. For example it can modify a class variable that will be applicable to all the instances.
+
+## Static Method
+
+- A static method is also a method which is bound to the class and not the object of the class.
+- A static method can’t access or modify class state.
+- It is present in a class because it makes sense for the method to be present in class.
+
+## *args and **kwargs
+
+- `*args` is used to pass an unlimted amount of arguments in the form of a list.
+- `**kwargs` like *args but the datastructure is a set `{}`
+
+
---
+++ RESTAPIUdemy/Python_Refresher/store_example.py
@@ -0,0 +1,31 @@
+class Store:
+    def __init__(self, name):
+        self.name = name
+        self.items = []
+
+    def add_item(self, name, price):
+        self.items.append({
+            'name': name,
+            'price': price
+        })
+
+    def stock_price(self):
+        total = 0
+        for item in self.items:
+            total += item['price']
+        return total
+
+    @classmethod
+    def franchise(cls, store):
+        # Return another store, with the same name as the argument's name, plus " - franchise"
+        # 2 options..
+        #return Store(store.name + '- franchise')
+        return cls(store.name + ' - franchise')
+
+    @staticmethod
+    def store_details(store):
+        # Return a string representing the argument
+        # It should be in the format 'NAME, total stock price: TOTAL'
+        # 2 options...
+        #return '{}, total stock price: {}'.format(store.name, int(store.stock_price()))
+        return store.name + ', total stock price: ' + str(store.stock_price())
