478106d373f9289353cfaafccfa250759065fc43
josephsmartinez <josephsmartinez@gmail.com>
2019-01-26 23:33:20 -0500

    simple api

---
+++ RESTAPIs/Flask/README.md
@@ -0,0 +1,21 @@
+# Flask Documentation
+
+## Quick Start
+
+[Flask quickstart](http://flask.pocoo.org/docs/1.0/quickstart/#a-minimal-application)
+
+## Flask Objects
+
+[Incoming Request Data¶](http://flask.pocoo.org/docs/1.0/api/#incoming-request-data)
+
+## Python Information
+
+[Style Guide](https://www.python.org/dev/peps/pep-0008/#whitespace-in-expressions-and-statements)
+
+Used for:  
+
+- Indenation
+
+## Python version
+
+[Make Aliases for bashrc](https://askubuntu.com/questions/320996/how-to-make-python-program-command-execute-python-3)
--- RESTAPIs/Flask/app.py
+++ RESTAPIs/Flask/app.py
@@ -1,6 +1,99 @@
-from flask import Flask 
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
+stores = [
+    {
+        'name' : 'Fast Food',
+        'items':[
+            {
+                'name':'Hotdog',
+                'price':'2.99'
+            }
+        ]
+    },
+    {
+        'name' : 'Coffee Shop',
+        'items':[
+            {
+                'name':'Coffee',
+                'price':'5.99'
+            }
+        ]
+    },
+    {
+        'name' : 'Pizza Shop',
+        'items':[
+            {
+                'name':'Pizza Pie',
+                'price':'15.99'
+            }
+        ]
+    }
+]
 
 
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+# POST - used to receive data
+# GET - used to send data back only
+
+#POST /store data: (name:)
+@app.route('/store', methods=['POST'])
+def create_store():
+  '''
+  Handle POST request when a new store item is received.
+  Return JSON object
+  '''
+  request_data = request.get_json()
+  new_store = {
+    'name': request_data['name'],
+    'items':[]
+  }
+  stores.append(new_store)
+  return jsonify(new_store) 
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+  for store in stores:
+    print(store)
+    if store['name'] == name:
+      return jsonify(store) 
+  return jsonify({"message": "store not found!"})
+
+# GET /store
+@app.route('/store')
+def get_stores():
+  return jsonify({'stores':stores})
+
+# POST /store/<string:name>/item {name:price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+  '''
+  Handle POST request when a new item is received.
+  Return JSON o
+  object
+  '''
+  request_data = request.get_json()
+  for store in stores:
+    if store['name'] == name:
+      new_item = {
+        'name': request_data['name'],
+        'price': request_data['price']
+      }
+      store['items'].append(new_item)
+      return jsonify(new_item)
+  return jsonify({'message': 'store not found'})
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+  for store in stores:
+    if store['name'] == name:
+      return jsonify({'items':store['items']}) 
+  return jsonify({"message": "store not found!"})
+
+app.debug=1
 app.run(port=5000)
