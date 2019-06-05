cf823719328ce99427f4b959cb246657349c1115
castic <castic@ubuntu19.ad.fiu.edu>
2019-06-05 17:29:26 -0400

    udpate

---
+++ DataVisualization/Arrays/creatingArrays.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+mylist1 = [1,2,3,4]
+myarray = np.array(mylist1)
+
+print(myarray)
+
+mylist2 = [5,6,7,8]
+mylist = [mylist1, mylist2]
+
+print(mylist)
+
+print(myarray.shape)
